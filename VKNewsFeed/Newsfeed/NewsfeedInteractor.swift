6eb87a7673d4f5bfe923e13de2d2166103935133
muk <muk863@rambler.ru>
2021-10-25 01:38:55 +0300

    added Loading Previous Posts

--- VKNewsFeed/Newsfeed/NewsfeedInteractor.swift
+++ VKNewsFeed/Newsfeed/NewsfeedInteractor.swift
@@ -9,45 +9,37 @@
 import UIKit
 
 protocol NewsfeedBusinessLogic {
-  func makeRequest(request: Newsfeed.Model.Request.RequestType)
+    func makeRequest(request: Newsfeed.Model.Request.RequestType)
 }
 
 class NewsfeedInteractor: NewsfeedBusinessLogic {
-
-  var presenter: NewsfeedPresentationLogic?
-  var service: NewsfeedService?
     
-    private var revealedPostIds = [Int]()
-    private var feedResponse: FeedResponse?
+    var presenter: NewsfeedPresentationLogic?
+    var service: NewsfeedService?
     
-    private var fetcher: DataFetcher = NetworkDataFetcher(networking: NetworkService())
-  
-  func makeRequest(request: Newsfeed.Model.Request.RequestType) {
-    if service == nil {
-      service = NewsfeedService()
-    }
-    
-    switch request {
-
-    case .getNewsfeed:
-        fetcher.getFeed { [weak self] (feedResponse) in
-            self?.feedResponse = feedResponse
-            self?.presentFeed()
+    func makeRequest(request: Newsfeed.Model.Request.RequestType) {
+        if service == nil {
+            service = NewsfeedService()
         }
-    case .revealPostIds(let postId):
-        revealedPostIds.append(postId)
-        presentFeed()
-    case .getUser:
-        fetcher.getUser { (userResponse) in
-            print(userResponse)
-            self.presenter?.presentData(response: Newsfeed.Model.Response.ResponseType.presentUserInfo(user: userResponse))
+        switch request {
+        case .getNewsfeed:
+            service?.getFeed(completion: { [weak self] (revealPostIds, feed) in
+                self?.presenter?.presentData(response: Newsfeed.Model.Response.ResponseType.presentNewsfeed(feed: feed, revealdedPostIds: revealPostIds))
+            })
+        case .getUser:
+            service?.getUser(completion: { [weak self] (user) in
+                self?.presenter?.presentData(response: Newsfeed.Model.Response.ResponseType.presentUserInfo(user: user))
+            })
+        case .revealPostIds(postId: let postId):
+            service?.revealPostIds(forPostId: postId, completion: { [weak self] (revealPostIds, feed) in
+                self?.presenter?.presentData(response: Newsfeed.Model.Response.ResponseType.presentNewsfeed(feed: feed, revealdedPostIds: revealPostIds))
+            })
+        case .getNextBatch:
+            print("123")
+            service?.getNextBatch(completion: { [weak self] (revealPostIds, feed) in
+                self?.presenter?.presentData(response: Newsfeed.Model.Response.ResponseType.presentNewsfeed(feed: feed, revealdedPostIds: revealPostIds))
+            })
         }
-    }
-  }
-    
-    private func presentFeed() {
-        guard let feedResponse = feedResponse else { return }
-        presenter?.presentData(response: Newsfeed.Model.Response.ResponseType.presentNewsfeed(feed: feedResponse, revealdedPostIds: revealedPostIds))
     }
 }
 
